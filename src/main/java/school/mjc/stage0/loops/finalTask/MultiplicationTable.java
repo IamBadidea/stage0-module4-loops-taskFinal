25bf126dac91188dfdb59477240ea90885fb8df7
Filipp <okruzhko.f@gmail.com>
2023-10-23 14:05:33 +0400

    feat: complete MultiplicationTable

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
+    public static void main(String[] args) {
+        new MultiplicationTable().printTable(99);
+    }
     public void printTable(int numberTableToPrint){
+
+        int i = 1;
+        while (i < 11) {
+            System.out.printf("%d x %d = %d\n", i, numberTableToPrint, i*numberTableToPrint);
+            i++;
+        }
     }
 }
