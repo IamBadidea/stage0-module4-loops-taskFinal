a7e20163293e1cc0f9d998525a5c46f118cc455b
Philip <okruzhko.f@gmail.com>
2023-10-23 19:54:06 +0400

    feat: complete Pyramid

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,9 +3,25 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength * 2 - 1; j++) {
+
+                if (cathetusLength - j <= i + 1 && j < cathetusLength) {
+                    System.out.print(cathetusLength - j);
+                } else if (j >= cathetusLength && j - cathetusLength + 2 <= i + 1) {
+                        System.out.print(j - cathetusLength + 2);
+                } else {
+                       System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(4);
     }
 }
